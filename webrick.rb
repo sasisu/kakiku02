3809c8f7bb268974bbb41710d29e8a5f35f68f0d
sasisu <syoyu.r@gmail.com>
2017-05-24 08:55:41 +0900

    Add files via upload

--- webrick.rb
+++ webrick.rb
@@ -2,12 +2,8 @@ require 'webrick'
 
 srv=WEBrick::HTTPServer.new({
    DocumentRoot: './',
-   BindAddress: '127.0.0.1',
-   Port: $PORT,
 })
 
-srv.mount('/',WEBrick::HTTPServlet::FileHandler,'index.html')
-   
 srv.start
 
 
